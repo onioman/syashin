631698b163131f34da5f947af1af9b3dac67893b
Antonio F. Lancho <onioman@gmail.com>
2015-01-08 06:56:35 +0900

    gallery: server files from media instead of as static files.

--- syashin/urls.py
+++ syashin/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +10,4 @@ urlpatterns = patterns('',
 
     url(r'^gallery/', include('gallery.urls', namespace="gallery")),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
